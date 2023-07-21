539ec35c00011ef6cdc66f933f8b731a63e75d64
Matheus <dsousamt@gmail.com>
2023-07-21 16:36:53 -0300

    resolve exercise 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...filters) {
 
+  let arrFilter = filters;
+  let base = arr;
+
+  let finalResult = base.filter(item => !arrFilter.includes(item))
+
+  return finalResult;
 };
 
 // Do not edit below this line
