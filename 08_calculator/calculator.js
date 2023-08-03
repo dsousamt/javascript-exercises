eb901948f9db30dcc458961ec9b707b7e155ec9d
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-08-03 16:51:06 -0300

    resolve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];    
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(n1, ...arr) {
+  let result = n1
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	let result = n1
+  for (let i = 1; i < n2; i++) {
+    result = n1 * result
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = null
+  if (n === 0 || n === 1) {
+    return 1
+  } else{
+    for (let i = 1; i < n; i++) {
+      result = n * factorial(n - 1)      
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
