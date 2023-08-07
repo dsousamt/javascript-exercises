c4765048c1f106e146fda5c47cca7c6f5f6efa97
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-08-07 17:01:27 -0300

    resolve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let str = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+  let reversedStr = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split('').reverse()
+  if (str == reversedStr.join('')) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
