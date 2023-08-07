4601e3d1a6b40001c83fdfaa06e039934f8811e1
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-08-07 18:17:25 -0300

    Resolve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+  if (count == 1 || count == 2) {
+    return 1
+  } else if ( count < 1){
+    return 'OOPS'
+  } else {
+    let fib = null
+    for (let i = 3; i <= count; i++) {
+      fib = fibonacci(i - 1) + fibonacci(i - 2)      
+    }
+    return fib
+  }
 };
 
 // Do not edit below this line
