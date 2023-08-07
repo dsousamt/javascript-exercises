c3074b64f22603ec37fdcfdd21dfbc540aea69d8
Matheus Sousa <34443498+dsousamt@users.noreply.github.com>
2023-08-07 18:54:28 -0300

    Resolve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let max = 0
+  let oldest = null
+  for (let i = 0; i < arr.length; i++) {
+    let age = null
+    if ('yearOfBirth' in arr[i] && 'yearOfDeath' in arr[i]) {
+      age = arr[i].yearOfDeath - arr[i].yearOfBirth      
+    } else {
+      let today = new Date()
+      age = today.getFullYear() - arr[i].yearOfBirth
+    }
+    if (age > max) {
+      max = age
+      oldest = arr[i]
+    }
+  }
+  return oldest
 };
 
 // Do not edit below this line
